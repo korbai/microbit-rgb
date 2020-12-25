2b80b5ccfe11d67b25aa0c7fb1c2a66e8671e8fe
Korbai, Zoltan <korbai.zoltan@gmail.com>
2020-12-25 15:33:03 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,6 +12,10 @@ input.onButtonPressed(Button.A, function () {
 // 1->0, 0->1
 input.onButtonPressed(Button.AB, function () {
     state = 1 - state
+    color = 0
+    red = 0
+    green = 0
+    blue = 0
 })
 input.onButtonPressed(Button.B, function () {
     if (0 == state) {
@@ -32,17 +36,35 @@ let state = 0
 let selected = 0
 selected = 0
 state = 0
-let color_options = ["R", "G", "B"]
 color = 0
-edubitRgbBit.setBrightness(32)
+red = 0
+green = 0
+blue = 0
+edubitRgbBit.setBrightness(16)
 // state=0: ledválasztás
 // state=1: színválasztás
 // 
 basic.forever(function () {
     if (0 == state) {
         basic.showNumber(selected)
-    } else {
-        basic.showString("" + (color_options[color]))
+        edubitTrafficLightBit.setLed(LedColor.Red, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+        edubitTrafficLightBit.setLed(LedColor.Green, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+        edubitTrafficLightBit.setLed(LedColor.Yellow, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+    }
+    if (state == 1) {
+        if (color == 0) {
+            edubitTrafficLightBit.setLed(LedColor.Red, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.On))
+            edubitTrafficLightBit.setLed(LedColor.Green, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+            edubitTrafficLightBit.setLed(LedColor.Yellow, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+        } else if (color == 1) {
+            edubitTrafficLightBit.setLed(LedColor.Red, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+            edubitTrafficLightBit.setLed(LedColor.Green, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.On))
+            edubitTrafficLightBit.setLed(LedColor.Yellow, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+        } else {
+            edubitTrafficLightBit.setLed(LedColor.Red, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+            edubitTrafficLightBit.setLed(LedColor.Green, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+            edubitTrafficLightBit.setLed(LedColor.Yellow, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.On))
+        }
     }
     if (state == 1) {
         edubitRgbBit.setPixelColor(selected, edubitRgbBit.rgb(red, green, blue))
